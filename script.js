af5f1d69a9202d73153b0cc4eb3446f3e0f04a69
Yuki <tranngochan2004.vn@gmail.com>
2026-09-20 20:38:31 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const card = document.querySelector('.card');
+const intro = document.querySelector('.intro');
+const invitation = document.querySelector('.invitation');
+const copyButton = document.querySelector(".copy-address");
+const address = document.querySelector(".location address");
+const copyIcon = document.querySelector(".copy-address img");
+
+card.addEventListener('click', () => {
+    intro.style.display = 'none';
+    invitation.style.display = 'grid';
+});
+
+copyButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(address.textContent);
+
+    copyIcon.src = "assets/icons/check.png";
+
+    setTimeout(() => {
+        copyIcon.src = "assets/icons/copy.png";
+    }, 1500);
+});
